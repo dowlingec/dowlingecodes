01e8f28cf1f9105ce2a49cd82930bb63ba1b378e
Elizabeth Dowling <dowlinge94@gmail.com>
2022-08-12 12:19:36 -0400

    active style fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ function NavBar() {
         margin: "0 6px 6px",
         // background: "blue",
         textDecoration: "none",
+        textShadow: "8px 8px 3px rgb(0, 0, 0)",
         color: "white",
         fontSize: "2em",
         textSizeAdjust: "auto"
@@ -16,16 +17,16 @@ function NavBar() {
     return (
         <div className="nav-bar">
             {/* <img src="" style={{ height: `100%`, marginLeft: `-2%`}}></img> */}
-                <NavLink activeClassName="active" to="/" exact style={linkStyles} activeStyle={{ background: "rgb(43, 13, 83)"}}>
+                <NavLink activeClassName="active" to="/" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
                 <i class="fa-solid fa-house"></i> Home
                 </NavLink>
-                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ background: "rgb(43, 13, 83)"}}>
+                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
                 <i class="fa-solid fa-align-center"></i> About
                 </NavLink>
-                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ background: "rgb(43, 13, 83)"}}>
+                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
                 <i class="fa-solid fa-folder-open"></i> Portfolio
                 </NavLink>
-                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ background: "rgb(43, 13, 83)"}}>
+                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
                 <i class="fa-solid fa-address-card"></i> Contact
                 </NavLink>
                 <hr></hr>
