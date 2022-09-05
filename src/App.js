d2116222d50c18e45690b2d0fb4651e6f3b44ad5
Elizabeth Dowling <dowlinge94@gmail.com>
2022-09-05 13:06:47 -0400

    color refresh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact"
 
-
 function App() {
   return (
     <div className="app-whole">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,8 +8,8 @@ function NavBar() {
         margin: "0 6px 6px",
         // background: "blue",
         textDecoration: "none",
-        textShadow: "8px 8px 3px rgb(0, 0, 0)",
-        color: "white",
+        // textShadow: "3px 3px 3px #fff",
+        color: "black",
         fontSize: "2em",
         textSizeAdjust: "auto"
       };
@@ -17,16 +17,16 @@ function NavBar() {
     return (
         <div className="nav-bar">
             {/* <img src="" style={{ height: `100%`, marginLeft: `-2%`}}></img> */}
-                <NavLink activeClassName="active" to="/" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
+                <NavLink activeClassName="active" to="/dowlingecodes" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
                 <i class="fa-solid fa-house"></i> Home
                 </NavLink>
-                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
+                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
                 <i class="fa-solid fa-align-center"></i> About
                 </NavLink>
-                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
+                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
                 <i class="fa-solid fa-folder-open"></i> Portfolio
                 </NavLink>
-                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ textShadow: "5px 5px 2px rgb(148, 16, 104)"}}>
+                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
                 <i class="fa-solid fa-address-card"></i> Contact
                 </NavLink>
                 <hr></hr>
