0f0081e44d40352bc489fe6c5372a4b1545c1cab
Elizabeth Dowling <dowlinge94@gmail.com>
2022-09-05 21:04:10 -0400

    menu appear on hover

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,19 +14,24 @@ function NavBar() {
         textSizeAdjust: "auto"
       };
 
+    // const navVisi = {
+    //     visibility: "visible"
+    // }
+    // onHover={navVisi}
+
     return (
         <div className="nav-bar">
             {/* <img src="" style={{ height: `100%`, marginLeft: `-2%`}}></img> */}
-                <NavLink activeClassName="active" to="/dowlingecodes" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
+                <NavLink activeClassName="active" to="/dowlingecodes" exact style={linkStyles} activeStyle={{ textShadow: "2px 2px 5px white"}}>
                 <i class="fa-solid fa-house"></i> Home
                 </NavLink>
-                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
+                <NavLink activeClassName="active" to="/about" exact style={linkStyles} activeStyle={{ textShadow: "2px 2px 5px white"}}>
                 <i class="fa-solid fa-align-center"></i> About
                 </NavLink>
-                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
+                <NavLink activeClassName="active" to="/portfolio" exact style={linkStyles} activeStyle={{ textShadow: "2px 2px 5px white"}}>
                 <i class="fa-solid fa-folder-open"></i> Portfolio
                 </NavLink>
-                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}}>
+                <NavLink activeClassName="active" to="/contact" exact style={linkStyles} activeStyle={{ textShadow: "2px 2px 5px white"}}>
                 <i class="fa-solid fa-address-card"></i> Contact
                 </NavLink>
                 <a href="https://docs.google.com/document/d/1iB2wZfiEI73TRKqixvNwM-io5t019rjMuoJJRYKSEoA/edit?usp=sharing" activeClassName="active" style={linkStyles} activeStyle={{ textShadow: "1.5px 1.5px 1.5px white"}} target="_blank"> <i class="fa-solid fa-file-arrow-down"></i> Resume</a>
