9ac708775ecc13b3db19f7d2c0a6a15cacbc88bc
Elizabeth Dowling <dowlinge94@gmail.com>
2022-10-22 13:35:50 -0400

    syntax update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,41 @@
 function Portfolio() {
-  const projects = {
+  let projects = [
+    {
+    "name": "Dresser",
+    "synopsis": "Weather-based outfit planning app",
+    "image": "",
+    "walkthroughURL": "Walkthrough coming soon",
+    "repoURL": "",
+    "details": "React.js front-end utilising SQLite backend on Ruby on Rails"
+    },
+    {
+      "name": "Genre Obscura",
+      "synopsis": "Early-internet nostalgic music bulletin board",
+      "image": "",
+      "walkthroughURL": `<iframe width="560" height="315" src="https://www.youtube.com/embed/Sfcj18rGYSI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      "repoURL": "",
+      "details": "Vanilla Javascript with CSS styling"
+      },
+    {
+      "name": "",
+      "synopsis": "",
+      "image": "",
+      "walkthroughURL": "",
+      "repoURL": "",
+      "details": ""
+    }
+  ]
+  console.log("Projects data", projects)
 
-  }
   return(
-    <div>
-        <h1>Loading projects...</h1>
-
+    <div class="project-body">
+      {projects.map((e) => {
+        return(
+        <div class="project-box">
+        <h2>{e.name}</h2>
+        </div>
+      )} 
+      )}
     </div>
   )  
 }
