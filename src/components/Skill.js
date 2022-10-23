9c8f9daf801bd4c7c634ebf193a74695c5b31c9e
Elizabeth Dowling <dowlinge94@gmail.com>
2022-10-22 22:25:33 -0400

    eight ball component NOT YET IN DIRECTORY

---
+++ src/components/Skill.js
@@ -0,0 +1,19 @@
+
+function Skill() {
+    const skills = [ "Team Leadership", "Initiative", "HTML", "JavaScript", "CSS", "React.js"]
+
+    return(
+        <div class="skills">
+            What's Liz's top skill?
+            <button 
+            // onClick={(e) => {
+                // e.preventDefault
+                // randomize number then display 
+            // }}
+            >???/TRY AGAIN</button>
+            <div class="eight-ball">
+                Answer goes here..
+            </div>
+        </div>
+    )
+}
