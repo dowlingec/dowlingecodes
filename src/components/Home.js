ced7d25d8969f2b79e0c3e6d0a9d158b1e65988b
Elizabeth Dowling <dowlinge94@gmail.com>
2023-12-29 15:42:30 -0500

    added learn more link

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ function Home() {
     return (
         <div className="home-whole">
             <h1>Elizabeth Dowling</h1>
-            <h3>&lt;/software-engineer></h3>
-            
+            <h3>&lt;/software-engineer&gt;</h3>
+            <br></br>
+            <h6>Learn more...</h6>
         </div>
     )
 }
